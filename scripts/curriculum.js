f2e907e0c77bf119faa3cb50f75fc14267241708
Dukky <alex.jieying.bloom@gmail.com>
2023-09-15 01:52:09 -0400

    Added courses
    Started using javascript
    added courses
    added button to reveal links to the course descriptions
    more formatting

---
+++ scripts/curriculum.js
@@ -0,0 +1,22 @@
+import courses from "/assets/courses.json" assert { type: 'json' };
+
+const courseList = document.getElementById("course-list-parent");
+const descriptionBtn = document.getElementById("description-btn");
+function addCourse(content) {
+    const newCourse = document.createElement("li");
+
+    newCourse.setAttribute("class", "course-list-child");
+    newCourse.innerHTML = content;
+
+    courseList.appendChild(newCourse);
+}
+
+
+
+descriptionBtn.addEventListener("click", (event) => {
+    for (let i in courses) {
+        let input = '<a class="course-description" href="' + courses.course[i].url + '">' + courses.course[i].name + '</a>';
+
+        addCourse(input)
+    }
+});
