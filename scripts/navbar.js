08ebeadabdb62b471651867cde243e883143bff3
Dukky <alex.jieying.bloom@gmail.com>
2023-09-18 22:11:18 -0400

    Added Diploma Page
    stuff

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -11,10 +11,4 @@ function scrollDown() {
     } else {
         navBar.classList.remove("sticky-header");
     }
-
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("header").style.fontSize = "30px";
-    } else {
-        document.getElementById("header").style.fontSize = "90px";
-    }
 }
