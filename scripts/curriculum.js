373d4dca81869add6de14ddbe125c77c059b90cb
Dukky <alex.jieying.bloom@gmail.com>
2023-09-26 22:45:15 -0400

    Fixed formatting
    added course descriptions

--- scripts/curriculum.js
+++ scripts/curriculum.js
@@ -2,19 +2,27 @@ import courses from "/assets/courses.json" assert { type: 'json' };
 
 const courseList = document.getElementById("course-list-parent");
 const descriptionBtn = document.getElementById("description-btn");
+const courseLinks = document.getElementsByClassName("course-list-child");
+
+let clicked = false;
+
 function addCourse(content) {
     const newCourse = document.createElement("li");
 
-    newCourse.setAttribute("class", "course-list-child");
+    newCourse.setAttribute("class", "course-list-child flex-child");
     newCourse.innerHTML = content;
 
     courseList.appendChild(newCourse);
 }
 
 descriptionBtn.addEventListener("click", (event) => {
-    for (let i in courses) {
-        let input = '<a class="course-description" href="' + courses[i].url + '">' + courses[i].name + '</a>';
+    if (!clicked) {
+        for (let i in courses) {
+            let input = '<a class="course-description" href="' + courses[i].url + '">' + courses[i].name + '</a>';
 
-        addCourse(input)
+            addCourse(input);
+        }
+
+        clicked = true;
     }
 });
